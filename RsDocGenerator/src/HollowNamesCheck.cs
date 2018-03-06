724dedb3c42b43d02971196f7543ccfed8f0dd45
Dmitry Matveev <dmitry.matveev@jetbrains.com>
2018-03-06 10:52:10 +0100

    2018.1 SDK

---
+++ RsDocGenerator/src/HollowNamesCheck.cs
@@ -0,0 +1,46 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Application.Settings;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.Tree;
+
+namespace RsDocGenerator
+{
+    [ElementProblemAnalyzer(typeof(IClassDeclaration),
+        HighlightingTypes = new []
+        {
+            typeof(HollowTypeNameHighlighting)
+        })]
+    public class HollowNamesCheck : ElementProblemAnalyzer<IClassDeclaration>
+    {
+        protected override void Run(IClassDeclaration element, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
+        {
+            var suffixes = GetSuffixes(data.SettingsStore);
+
+            var match = GetFirstMatchOrDefault(element.DeclaredName, suffixes);
+            if (match != null)
+                AddHighlighting(match, consumer, element);
+        }
+
+        private IEnumerable<string> GetSuffixes(IContextBoundSettingsStore dataSettingsStore)
+        {
+            var suffixes = "crap, stuff";
+            return suffixes.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private static string GetFirstMatchOrDefault(string declaredName, IEnumerable<string> suffixes)
+        {
+            return suffixes.FirstOrDefault(declaredName.EndsWith);
+        }
+
+        private void AddHighlighting(string bannedSuffix, IHighlightingConsumer consumer, IClassDeclaration typeExpression)
+        {
+            var identifier = typeExpression.NameIdentifier;
+            var documentRange = identifier.GetDocumentRange();
+            var highlighting = new HollowTypeNameHighlighting("This is my tooltip", documentRange);
+            consumer.AddHighlighting(highlighting);
+        }
+    }
+}*/
---
+++ RsDocGenerator/src/HollowTypeNameHighlighting.cs
@@ -0,0 +1,47 @@
+﻿/*using JetBrains.Application;
+using JetBrains.DocumentModel;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Feature.Services.Explanatory;
+using JetBrains.ReSharper.Psi.CSharp;
+using RsDocGenerator;
+
+[assembly: RegisterConfigurableSeverity(HollowTypeNameHighlighting.SeverityID, null, 
+    HighlightingGroupIds.CodeSmell, "Hollow type name", "This type has a name that doesn't express its intent.",
+    Severity.SUGGESTION)]
+
+namespace RsDocGenerator
+{
+    [ConfigurableSeverityHighlighting(SeverityID, CSharpLanguage.Name)]
+    public class HollowTypeNameHighlighting : IHighlighting
+    {
+        internal const string SeverityID = "HollowTypeName";
+        private readonly DocumentRange documentRange;
+
+        public HollowTypeNameHighlighting(string toolTip, DocumentRange documentRange)
+        {
+            ToolTip = toolTip;
+            this.documentRange = documentRange;
+        }
+
+        public DocumentRange CalculateRange() => documentRange;
+        public string ToolTip { get; }
+        public string ErrorStripeToolTip => ToolTip;
+        public bool IsValid() => true;
+    }
+    
+    [ShellComponent]
+    public class MyHelpIdProvider : ICodeInspectionWikiDataProvider
+    {
+        public bool TryGetValue(string attributeId, out string url)
+        {
+            url = "yyy";
+            if (attributeId == HollowTypeNameHighlighting.SeverityID)
+            {
+                url = "https://www.jetbrains.com/help/resharper/sdk/HowTo/AnalyzeCode/AnalyzeCodeOnTheFly.html";
+                return true;
+            }
+
+            return false;
+        }
+    }
+}*/
